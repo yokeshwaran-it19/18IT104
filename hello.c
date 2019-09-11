72674b3118ee65b26617ee700c231c8550620bc0
yokeshwaran-it19 <yokeshwaran.it19@bitsathy.ac.in>
2019-09-11 18:27:18 +0530

    hello.c

--- hello.c
+++ hello.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 void main()
 {
-printf("HELLO");
+printf("HELLO sir \n my NAME is YOKESHWARAN and I am from 'IT' department\n");
+printf("@ \n@ @\n@ @ @\n@ @ @ @\n");
+printf("      1\n    1 2 1\n  1 2 3 2 1\n1 2 3 4 3 2 1\n");
+printf("ADDING TWO NUMBERS WITHOUT ASKING THE INPUT 4+1=%d\n",4+1);
+printf("MULTIPLE OPERATORS FUNCTION 25-5=%d 25*5=%d 25/5=%d\n",25-5,25*5,25/5);
+printf("FOR GETTING THE CUT OFF VALUE %d\n",69+72/2+75/2);
 }
